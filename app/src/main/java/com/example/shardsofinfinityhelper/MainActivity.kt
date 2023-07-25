62b4ba41528ea03985948f1ea83aed094d024524
kjemison <kurtisjemison@gmail.com>
2023-07-25 17:54:48 -0500

    Add working value displays

--- app/src/main/java/com/example/shardsofinfinityhelper/MainActivity.kt
+++ app/src/main/java/com/example/shardsofinfinityhelper/MainActivity.kt
@@ -17,111 +17,156 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-val iconSizeModifier = Modifier
-	.size(180.dp)
+const val iconSize: Int = 180
 val arrowHeightModifier = Modifier
 	.height(45.dp)
-
-val player1 = Player()
-val player2 = Player()
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			PlayerScreen(player = player1)
-			PlayerScreen(player = player2)
+			PlayerScreen()
 		}
 	}
 }
 
 @Composable
-fun PlayerScreen(player: Player) {
-	Column(modifier = Modifier.fillMaxSize()) {
-		Row(modifier = Modifier
-			.height(300.dp)
-			.fillMaxWidth(),
-			horizontalArrangement = Arrangement.SpaceEvenly
+fun PlayerScreen() {
+	Column(
+		modifier = Modifier.fillMaxSize(),
+		verticalArrangement = Arrangement.SpaceBetween
+	) {
+		val player1 = Player("player1")
+		val player2 = Player("player2")
+
+		PlayerArea(player = player1)
+		PlayerArea(player = player2)
+	}
+}
+
+@Composable
+fun PlayerArea(player: Player, modifier: Modifier = Modifier) {
+	Box {
+		Row(
+			horizontalArrangement = Arrangement.SpaceEvenly,
+			modifier = Modifier
+				.height(300.dp)
+				.fillMaxWidth()
 		) {
+			var playerMastery by remember { mutableStateOf(player.mastery) }
+			var playerHealth by remember { mutableStateOf(player.health) }
 			Column(
 				modifier = Modifier
 					.width(200.dp)
 					.fillMaxHeight(),
 				horizontalAlignment = Alignment.CenterHorizontally
 			) {
-				UpArrow(player, "Health")
-				Box {
-					Image(
-						painter = painterResource(id = R.drawable.shards_of_infinity_hp),
-						contentDescription = null,
-						modifier = iconSizeModifier
-					)
-					Text(
-						text = "${player.health}",
-						fontSize = 50.sp,
-						modifier = Modifier.align(Alignment.Center)
-					)
+				Arrow(R.drawable.baseline_keyboard_arrow_up_24) {
+					if (playerHealth != 50) playerHealth += 1
 				}
-				DownArrow(player, "Health")
+				Box {
+					HealthIcon()
+					if (player.name == "player1") {
+						PlayerValue(
+							playerHealth,
+							Modifier
+								.align(Alignment.Center)
+								.rotate(180f)
+						)
+					} else {
+						PlayerValue(playerHealth, Modifier.align(Alignment.Center))
+					}
+				}
+				Arrow(R.drawable.baseline_keyboard_arrow_down_24) {
+					if (playerHealth != 0) playerHealth -= 1
+				}
 			}
 			Column(
 				modifier = Modifier.fillMaxHeight(),
 				horizontalAlignment = Alignment.CenterHorizontally)
 			{
-				UpArrow(player, "Mastery")
-				Box {
-					Image(
-						painter = painterResource(id = R.drawable.shards_of_infinity_mastery),
-						contentDescription = null,
-						modifier = iconSizeModifier
-					)
-					Text(
-						text = "${player.mastery}",
-						fontSize = 50.sp,
-						modifier = Modifier.align(Alignment.Center)
-					)
+				Arrow(R.drawable.baseline_keyboard_arrow_up_24) {
+					if (playerMastery != 30) playerMastery += 1
 				}
-				DownArrow(player, "Mastery")
+				Box {
+					MasteryIcon()
+					if (player.name == "player1") {
+						PlayerValue(
+							playerMastery,
+							Modifier
+								.align(Alignment.Center)
+								.rotate(180f))
+					} else {
+						PlayerValue(playerMastery, Modifier.align(Alignment.Center))
+					}
+				}
+				Arrow(R.drawable.baseline_keyboard_arrow_down_24) {
+					if (playerMastery != 0) playerMastery -= 1
+				}
 			}
 		}
 	}
 }
 
 @Composable
-fun UpArrow(player: Player, actionType: String) {
-	Image(
-		painter = painterResource(id = R.drawable.baseline_keyboard_arrow_up_24),
-		contentDescription = null,
-		modifier = arrowHeightModifier
-			.fillMaxWidth()
-			.clickable {
-				when (actionType) {
-					"Health" -> player.upHealth()
-					"Mastery" -> player.upMastery()
-				}
-			}
+fun PlayerValue(value: Int, modifier: Modifier = Modifier) {
+	println("player value: ${value}")
+	Text(
+		text = "$value",
+		fontSize = 50.sp,
+		modifier = modifier
 	)
 }
 
 @Composable
-fun DownArrow(player: Player, actionType: String) {
+fun PlayerBanner() {
 	Image(
-		painter = painterResource(id = R.drawable.baseline_keyboard_arrow_down_24),
+		painter = painterResource(id = R.drawable.shards_banner),
+		contentDescription = null,
+		modifier = Modifier.fillMaxSize(),
+		alignment = Alignment.TopCenter
+	)
+}
+
+@Composable
+fun HealthIcon(modifier: Modifier = Modifier) {
+	Image(
+		painter = painterResource(id = R.drawable.shards_of_infinity_hp),
+		contentDescription = null,
+		modifier.size(iconSize.dp)
+	)
+}
+
+@Composable
+fun MasteryIcon(modifier: Modifier = Modifier) {
+	Image(
+		painter = painterResource(id = R.drawable.shards_of_infinity_mastery),
+		contentDescription = null,
+		modifier.size(iconSize.dp)
+	)
+}
+
+@Composable
+fun Arrow(paint: Int, onClick: () -> Unit) {
+	Image(
+		painter = painterResource(id = paint),
 		contentDescription = null,
 		modifier = arrowHeightModifier
 			.fillMaxWidth()
 			.clickable {
-				when (actionType) {
-					"Health" -> player.downHealth()
-					"Mastery" -> player.downMastery()
-				}
+				onClick()
 			}
 	)
 }
@@ -129,5 +174,5 @@ fun DownArrow(player: Player, actionType: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-	PlayerScreen(player1)
+	PlayerScreen()
 }
--- app/src/main/java/com/example/shardsofinfinityhelper/Player.kt
+++ app/src/main/java/com/example/shardsofinfinityhelper/Player.kt
@@ -1,35 +1,8 @@
 package com.example.shardsofinfinityhelper
 
 data class Player(
+	val name: String,
 	var health: Int = 50,
 	var mastery: Int = 0,
 	var levelledThisTurn: Boolean = false
-) {
-	fun upHealth() {
-		if (health == 50) {
-			return
-		}
-		health++
-	}
-
-	fun downHealth() {
-		if (health == 0) {
-			return
-		}
-		health--
-	}
-
-	fun upMastery() {
-		if (mastery == 50) {
-			return
-		}
-		mastery++
-	}
-
-	fun downMastery() {
-		if (mastery == 0) {
-			return
-		}
-		mastery--
-	}
-}
+ )

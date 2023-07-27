b4601be0f11441e66a1ffd73e86646f5837e5315
kjemison <kurtisjemison@gmail.com>
2023-07-26 19:09:35 -0500

    Add mastery tracker

--- app/src/main/java/com/example/shardsofinfinityhelper/MainActivity.kt
+++ app/src/main/java/com/example/shardsofinfinityhelper/MainActivity.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,8 +26,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,84 +53,62 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun PlayerScreen() {
 	Column(
-		modifier = Modifier.fillMaxSize(),
-		verticalArrangement = Arrangement.SpaceBetween
+		verticalArrangement = Arrangement.SpaceBetween,
+		modifier = Modifier
+			.fillMaxSize()
+			.background(color = colorResource(id = R.color.background))
 	) {
 		val player1 = Player("player1")
 		val player2 = Player("player2")
 
-		PlayerArea(player = player1)
+		PlayerArea(player = player1, modifier = Modifier.rotate(180f))
 		PlayerArea(player = player2)
 	}
 }
 
 @Composable
 fun PlayerArea(player: Player, modifier: Modifier = Modifier) {
-	Box {
+	Column(
+		modifier = modifier,
+		verticalArrangement = Arrangement.Center
+	) {
 		Row(
 			horizontalArrangement = Arrangement.SpaceEvenly,
 			modifier = Modifier
 				.height(300.dp)
 				.fillMaxWidth()
 		) {
-			var playerMastery by remember { mutableStateOf(player.mastery) }
-			var playerHealth by remember { mutableStateOf(player.health) }
-			Column(
-				modifier = Modifier
-					.width(200.dp)
-					.fillMaxHeight(),
-				horizontalAlignment = Alignment.CenterHorizontally
-			) {
-				Arrow(R.drawable.baseline_keyboard_arrow_up_24) {
-					if (playerHealth != 50) playerHealth += 1
-				}
-				Box {
-					HealthIcon()
-					if (player.name == "player1") {
-						PlayerValue(
-							playerHealth,
-							Modifier
-								.align(Alignment.Center)
-								.rotate(180f)
-						)
-					} else {
-						PlayerValue(playerHealth, Modifier.align(Alignment.Center))
-					}
-				}
-				Arrow(R.drawable.baseline_keyboard_arrow_down_24) {
-					if (playerHealth != 0) playerHealth -= 1
-				}
-			}
-			Column(
-				modifier = Modifier.fillMaxHeight(),
-				horizontalAlignment = Alignment.CenterHorizontally)
-			{
-				Arrow(R.drawable.baseline_keyboard_arrow_up_24) {
-					if (playerMastery != 30) playerMastery += 1
-				}
-				Box {
-					MasteryIcon()
-					if (player.name == "player1") {
-						PlayerValue(
-							playerMastery,
-							Modifier
-								.align(Alignment.Center)
-								.rotate(180f))
-					} else {
-						PlayerValue(playerMastery, Modifier.align(Alignment.Center))
-					}
-				}
-				Arrow(R.drawable.baseline_keyboard_arrow_down_24) {
-					if (playerMastery != 0) playerMastery -= 1
-				}
-			}
+			HealthIconWithInfo(player = player, modifier)
+			MasteryIconWithInfo(player = player, modifier)
+
+		}
+		Row(
+			modifier = modifier
+				.fillMaxWidth()
+				.padding(top = 20.dp, bottom = 20.dp),
+			horizontalArrangement = Arrangement.Center
+		) {
+			var selected by remember { mutableStateOf(false) }
+			Checkbox(
+				modifier = modifier
+					.scale(1.5f)
+					.align(CenterVertically),
+				checked = selected,
+				onCheckedChange = { selected = it }
+			)
+			Spacer(modifier = Modifier.width(30.dp))
+			Text(
+				modifier = modifier.align(CenterVertically),
+				text = "Mastery gained?",
+				color = Color.White,
+				fontSize = 25.sp
+			)
 		}
 	}
 }
 
 @Composable
 fun PlayerValue(value: Int, modifier: Modifier = Modifier) {
-	println("player value: ${value}")
 	Text(
 		text = "$value",
 		fontSize = 50.sp,
@@ -131,13 +117,85 @@ fun PlayerValue(value: Int, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun PlayerBanner() {
-	Image(
-		painter = painterResource(id = R.drawable.shards_banner),
-		contentDescription = null,
-		modifier = Modifier.fillMaxSize(),
-		alignment = Alignment.TopCenter
+fun HealthIconWithInfo(player: Player, modifier: Modifier = Modifier) {
+	var playerHealth by remember {
+		mutableStateOf(player.health)
+	}
+	Column(
+		modifier = modifier
+			.width(200.dp)
+			.fillMaxHeight(),
+		horizontalAlignment = Alignment.CenterHorizontally
+	) {
+		Arrow(R.drawable.baseline_keyboard_arrow_up_24_red) {
+			if (player.name == "player1") {
+				if (playerHealth != 0) playerHealth -= 1
+			} else {
+				if (playerHealth != 50) playerHealth += 1
+			}
+		}
+		Box {
+			HealthIcon()
+			if (player.name == "player1") {
+				PlayerValue(
+					playerHealth,
+					Modifier
+						.align(Alignment.Center)
+						.rotate(180f)
+				)
+			} else {
+				PlayerValue(playerHealth, modifier.align(Alignment.Center))
+			}
+		}
+		Arrow(R.drawable.baseline_keyboard_arrow_down_24_red) {
+			if (player.name == "player1") {
+				if (playerHealth != 50) playerHealth += 1
+			} else {
+				if (playerHealth != 0) playerHealth -= 1
+			}
+		}
+	}
+}
+
+@Composable
+fun MasteryIconWithInfo(player: Player, modifier: Modifier = Modifier) {
+	var playerMastery by remember {
+		mutableStateOf(player.mastery)
+	}
+	Column(
+		modifier = modifier
+			.width(200.dp)
+			.fillMaxHeight(),
+		horizontalAlignment = Alignment.CenterHorizontally
 	)
+	{
+		Arrow(R.drawable.baseline_keyboard_arrow_up_24_red) {
+			if (player.name == "player1") {
+				if (playerMastery != 0) playerMastery -= 1
+			} else {
+				if (playerMastery != 30) playerMastery += 1
+			}
+		}
+		Box {
+			MasteryIcon()
+			if (player.name == "player1") {
+				PlayerValue(
+					playerMastery,
+					Modifier
+						.align(Alignment.Center)
+						.rotate(180f))
+			} else {
+				PlayerValue(playerMastery, modifier.align(Alignment.Center))
+			}
+		}
+		Arrow(R.drawable.baseline_keyboard_arrow_down_24_red) {
+			if (player.name == "player1") {
+				if (playerMastery != 30) playerMastery += 1
+			} else {
+				if (playerMastery != 0) playerMastery -= 1
+			}
+		}
+	}
 }
 
 @Composable
@@ -159,7 +217,7 @@ fun MasteryIcon(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Arrow(paint: Int, onClick: () -> Unit) {
+fun Arrow(paint: Int, modifier: Modifier = Modifier, onClick: () -> Unit) {
 	Image(
 		painter = painterResource(id = paint),
 		contentDescription = null,
